2550db1bedfcb1c1f7b6dfbc7c5df48da9446fe5
Bhoomi Kathiriya <bhoomi.kathiriya@ecosmob.com>
2020-02-19 13:14:42 +0530

    Added custom view with button Click

---
+++ Temp/CustomView.swift
@@ -0,0 +1,28 @@
+//
+//  CustomView.swift
+//  Temp
+//
+//  Created by Jaimin Modi on 19/02/20.
+//  Copyright © 2020 Jaimin Modi. All rights reserved.
+//
+
+import UIKit
+
+class CustomView: UIView {
+
+    
+    @IBAction func dismissButtonClick(_ sender: Any) {
+        print("dismissButtonClick") // not called
+       // self.completion?()
+       // self.removeFromSuperview()
+    }
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Temp/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  Temp
+//
+//  Created by Jaimin Modi on 19/02/20.
+//  Copyright © 2020 Jaimin Modi. All rights reserved.
+//
+
+import UIKit
+
+
+extension UIView {
+    class func fromNib<T: UIView>() -> T {
+        return Bundle(for: T.self).loadNibNamed(String(describing: T.self), owner: nil, options: nil)![0] as! T
+    }
+}
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var viewNav :  CustomView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        viewNav  = UIView.fromNib()
+        viewNav.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: viewNav.frame.size.height)
+        
+        self.view.addSubview(viewNav)
+
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
